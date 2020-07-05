1fdeb20443cfceddce9d0459abcde532d4b8f007
phan phan <manhphuoc1995@gmail.com>
2020-07-05 21:09:02 +0700

    Add Log Seed Data Create account

---
+++ FSLogistic.Core/Helpers/ValidationHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace FSLogistic.Core.Helpers
+{
+    public static class ValidationHelper
+    {
+        public static bool CheckExisted<T>(T value, IEnumerable<T> listChecking)
+        {
+            return listChecking.Any(x => x.Equals(value));
+        }
+    }
+}
