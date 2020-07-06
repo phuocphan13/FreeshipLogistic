d6337c910a83d983317fa5de9a63aba62bfcaff8
NGUYỄN ĐÌNH DUY TÀI <tai.nguyendinhduy@gmail.com>
2020-07-06 09:06:14 +0700

    Initial commit

---
+++ FSLogistic.Web/ClientApp/src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { Routes, RouterModule } from "@angular/router";
+import { NgModule } from "@angular/core";
+import { AuthorizeGuard } from "src/api-authorization/authorize.guard";
+
+
+const appRoutes: Routes = [
+    {
+        path: 'shipper',
+        canActivate: [AuthorizeGuard],
+        loadChildren: './shipper/shipper.module#ShipperModule'
+    },
+    {
+
+        path: 'admin',
+        canActivate: [AuthorizeGuard],
+        loadChildren: './admin/admin.module#AdminModule'
+    },
+
+
+
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+
+export class AppRoutingModule { }
--- FSLogistic.Web/ClientApp/src/app/app.module.ts
+++ FSLogistic.Web/ClientApp/src/app/app.module.ts
@@ -2,52 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
-import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
-import { ProductComponent } from './product/product.component';
 import { ServiceModule } from 'src/core/services/service.module';
-import { TestFlowComponent } from './test-flow/test-flow.component';
-import { CustomerViewComponent } from './customer-view/customer-view.component';
-import { CustomerDataViewComponent } from './customer-data-view/customer-data-view.component';
-import { CustomerToolbarComponent } from './customer-toolbar/customer-toolbar.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
-    ProductComponent,
-    TestFlowComponent,
-    CustomerViewComponent,
-    CustomerDataViewComponent,
-    CustomerToolbarComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-
+    AppRoutingModule,
     ApiAuthorizationModule,
     ServiceModule,
-    
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard] },
-      { path: 'product', component: ProductComponent, canActivate: [AuthorizeGuard] },
-      { path: 'customer', component: CustomerViewComponent },
-      { path: 'test-flow', component: TestFlowComponent, canActivate: [AuthorizeGuard] }
-    ])
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthorizeInterceptor, multi: true }
