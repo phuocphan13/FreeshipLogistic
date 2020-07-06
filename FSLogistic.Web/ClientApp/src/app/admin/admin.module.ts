ba353bb98f47d024ae4db6c2a5b0a6dc8b1c827f
NGUYỄN ĐÌNH DUY TÀI <tai.nguyendinhduy@gmail.com>
2020-07-06 11:02:57 +0700

    Story310-Task317 FE functions FIX module

--- FSLogistic.Web/ClientApp/src/app/admin/admin.module.ts
+++ FSLogistic.Web/ClientApp/src/app/admin/admin.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { ShipperManagementModule } from './shipper-management/shipper-management.module';
 import { AdminComponent } from './admin.component'
+import { AdminBillModule } from './admin-bill/admin-bill.module';
 
 @NgModule({
   declarations: [
@@ -11,11 +12,13 @@ import { AdminComponent } from './admin.component'
   imports: [
     CommonModule,
     ShipperManagementModule,
+    AdminBillModule,
     AdminRoutingModule,
   ],
   exports: [
     AdminComponent,
     ShipperManagementModule,
+    AdminBillModule,
   ]
 })
 export class AdminModule { }
