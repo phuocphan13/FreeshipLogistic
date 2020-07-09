3928e077b1ef104ad5dcd000994bf4b9dd65a1bd
manhtai2211 <manhtai2211@gmail.com>
2020-07-09 23:24:58 +0700

    Fix_task340_Toolbar-shipper Fix_task315_UIDesign

--- FSLogistic.Web/ClientApp/src/app/shipper/shipper-bill/toolbar-shipper/toolbar-shipper.component.ts
+++ FSLogistic.Web/ClientApp/src/app/shipper/shipper-bill/toolbar-shipper/toolbar-shipper.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar-shipper',
@@ -17,6 +17,18 @@ export class ToolbarShipperComponent implements OnInit {
     { id: 1, name: 'Rồi' },
     { id: 2, name: 'Chưa' },
     { id: 3, name: 'Chuyển hoàn' },
+  ];
+
+  customerId: any;
+  pickedCustomer: any;
+
+  listCustomer: any = [
+    { id: 1, name: 'NguyenThu' },
+    { id: 2, name: 'KhoGaChiBuoi' },
+    { id: 3, name: 'LeMinh' },
+    { id: 4, name: 'MyTra' },
+    { id: 5, name: 'UyenBetty' },
+    { id: 6, name: 'ThanhVy' },
   ];
 
   constructor() { }
