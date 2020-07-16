ab2eb9f0460d00de24c8795faed266a81c0df549
duytaiboi <45912835+duytaiboi@users.noreply.github.com>
2020-07-16 17:04:25 +0700

    Story303-Task386 BE Service + Message constant

---
+++ FSLogistic.Service/ShipperManagement/IShipperManagementService.cs
@@ -0,0 +1,15 @@
+﻿using FSLogistic.Model.Account;
+using FSLogistic.Model.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSLogistic.Service.ShipperManagement
+
+{
+    public interface IShipperManagementService
+    {
+        Task<ResponeModel<List<ShipperAccountModel>>> GetListAccount();
+    }
+}
---
+++ FSLogistic.Service/ShipperManagement/ShipperManagementService.cs
@@ -0,0 +1,55 @@
+﻿using FSLogistic.Core.Consts;
+using FSLogistic.Core.Repositories;
+using FSLogistic.Core.UoW;
+using FSLogistic.Model.Account;
+using FSLogistic.Model.Shared;
+using FSLogistic.Service.Account;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSLogistic.Service.ShipperManagement
+{
+    public class ShipperManagementService : BaseService, IShipperManagementService
+    {
+
+        public ShipperManagementService(IPrincipal principal, IHttpContextAccessor context,
+            IRepository<Domain.Models.Account> accountRepository 
+           ) : base(principal, context, accountRepository)
+        {
+        }
+
+        public async Task<ResponeModel<List<ShipperAccountModel>>> GetListAccount()
+        {
+            var listResourceAccountsEntity = await _accountRepository.GetAll().ToListAsync();
+            if(listResourceAccountsEntity==null)
+            {
+                return SetResponeData<List<ShipperAccountModel>>(ShipperManagementMessage.ErrorMessageLoadData, ResponeStatusEnum.Null, null);
+            }
+
+            var listShipperAccountModel = new List<ShipperAccountModel>();
+            foreach(var item in listResourceAccountsEntity)
+            {
+                var accountmodel = new ShipperAccountModel()
+                {
+                    Code = item.Code,
+                    Name = item.Name,
+                    UserId = item.UserId,
+                    UserName = "hardcode@gmail.com",
+                };
+                listShipperAccountModel.Add(accountmodel);
+            }          
+
+            return SetResponeData(ShipperManagementMessage.GetSuccessed, ResponeStatusEnum.Successed, listShipperAccountModel);
+        }
+
+
+
+    }
+}
