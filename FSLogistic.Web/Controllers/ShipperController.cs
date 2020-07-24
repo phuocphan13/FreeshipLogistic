d718573b2540b6e37cc259407a938a353278093b
manhtai2211 <manhtai2211@gmail.com>
2020-07-24 17:32:45 +0700

    commit_BackEnd_API

---
+++ FSLogistic.Web/Controllers/ShipperController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FSLogistic.Service.Bill_Shipper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FSLogistic.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShipperController : ControllerBase
+    {
+        private readonly IShipperService _shipperService;
+
+        public ShipperController(IShipperService shipperService)
+        {
+            _shipperService = shipperService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await _shipperService.GetDataShipper();
+            return Ok(result);
+        }
+    }
+}
