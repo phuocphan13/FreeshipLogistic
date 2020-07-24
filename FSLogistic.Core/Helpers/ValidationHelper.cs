39e6a6d477bd309add6ae021256606e4e0152909
kimocheese.kimochi@gmail.com <thuanphat0611@gmail.com>
2020-07-24 19:45:30 +0700

    Add Validate for Name + Check Null + Check Number

--- FSLogistic.Core/Helpers/ValidationHelper.cs
+++ FSLogistic.Core/Helpers/ValidationHelper.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Text;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Reflection;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace FSLogistic.Core.Helpers
 {
@@ -23,6 +25,20 @@ namespace FSLogistic.Core.Helpers
             return true;
         }
 
+        public static bool ValidationForName(string data)
+        {
+            var regularExpression = @"^[a-zA-Z aàảãáạăằẳẵắặâầẩẫấậbcdđĐeèẻẽéẹêềểễếệfghiìỉĩíịjklmnoòỏõóọôồổỗốộơờởỡớợpqrstuùủũúụưừửữứựvwxyỳỷỹýỵz]+$";
+            var r = new Regex(regularExpression);
+            return r.IsMatch(data);
+        }
+
+        public static bool ValidationForCode(string data)
+        {
+            var regularExpression = @"^[a-zA-Z0-9-_]+$";
+            var r = new Regex(regularExpression);
+            return r.IsMatch(data);
+        }
+
         public static bool ValidationForNumber(string data)
         {
             var isNumber = decimal.TryParse(data, out decimal result);
@@ -34,7 +50,31 @@ namespace FSLogistic.Core.Helpers
             }
             return false;
         }
+
+        public static bool CheckNullObject<T>(T data) where T : class
+        {
+            foreach (PropertyInfo propertyInfo in data.GetType().GetProperties())
+            {
+                if (propertyInfo.GetValue(data, null) == null)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool CheckNumberObject(object data)
+        {
+            foreach (PropertyInfo propertyInfo in data.GetType().GetProperties())
+            {
+                var propertyValue = propertyInfo.GetValue(data).ToString();
+                var regularExpression = @"^[0-9]*$";
+                var r = new Regex(regularExpression);
+                var isMatched = r.IsMatch(propertyValue);
+                if (!isMatched)
+                    return false;
+            }
+            return true;
+        }
     }
-
-
 }
