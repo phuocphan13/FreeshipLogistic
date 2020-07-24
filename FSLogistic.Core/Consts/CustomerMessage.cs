5acbab1d9a6dfa2d03c10e26d68e34bb2e1e5789
kimocheese.kimochi@gmail.com <thuanphat0611@gmail.com>
2020-07-24 19:44:16 +0700

    fix Const + Message

--- FSLogistic.Core/Consts/CustomerMessage.cs
+++ FSLogistic.Core/Consts/CustomerMessage.cs
@@ -6,8 +6,11 @@ namespace FSLogistic.Core.Consts
 {
     public static class CustomerMessage
     {
-        public const string ErrorMessageLoadData = "Không có dữ liệu, hiển thị thất bại. \n ";
-        public const string ErrorMessageCreate = "Khách hàng này đã có rồi, tạo khách hàng thất bại. \n ";
-        public const string SuccessedMessageCreate = "Thêm khách hàng thành công. \n ";
+        public const string CustomerGetSuccessedMessage = "Lấy dữ liệu thành công. \n";
+        public const string CustomerGetFailedMessage = "Lấy dữ liệu thất bại. \n ";
+        public const string CustomerExistedMessage = "Khách hàng này đã tồn tại. \n ";
+        public const string CustomerCreateFailedMessage = "Dữ liệu đã nhập không đúng định dạng, vui lòng kiểm tra lại. \n ";
+        public const string CustomerCreateSuccessedMessage = "Thêm khách hàng thành công. \n ";
+        public const string CustomerSaveFailedMessage = "Thêm khách hàng thất bại. \n ";
     }
 }
