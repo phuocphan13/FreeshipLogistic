729a971de091d0b76bc7d7d342bcee7b0affc7f3
kimocheese.kimochi@gmail.com <thuanphat0611@gmail.com>
2020-08-02 17:09:42 +0700

    add alert service + add update function

--- FSLogistic.Web/ClientApp/src/app/admin/customer-management/customer-management-datashowing/customer-management-datashowing.component.ts
+++ FSLogistic.Web/ClientApp/src/app/admin/customer-management/customer-management-datashowing/customer-management-datashowing.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Customer } from 'src/core/models/customer.model';
 import { CustomerService } from 'src/core/services/customer.service';
 import { ResponeStatusEnum } from 'src/core/models/shared/respone.model';
 import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
 import { AddCustomerModalComponent } from 'src/app/modals/add-customer-modal/add-customer-modal.component';
+import { UpdateCustomerModalComponent } from 'src/app/modals/update-customer-modal/update-customer-modal.component';
+import { AlertMessageService } from 'src/core/services/alert-message.service';
+import { AlertService } from 'ngx-alerts';
+import { CustomerType } from 'src/app/common/customer-enum';
 
 @Component({
   selector: 'app-customer-management-datashowing',
@@ -11,30 +15,81 @@ import { AddCustomerModalComponent } from 'src/app/modals/add-customer-modal/add
   styleUrls: ['./customer-management-datashowing.component.scss'],
 })
 export class CustomerManagementDatashowingComponent implements OnInit {
+
+  isExpand: boolean = false;
+  detailText: string = "Xem ＋";
+
   listCustomers: Customer[] = [];
+  listCustomersShow: Customer[] = [];
+
+  statusId: any;
+
+  searchData: any;
+
+  listCustomersSearch: Customer[] = [];
 
   modalRef: BsModalRef;
-  config: ModalOptions = { class: 'modal-lg' };
+  config: ModalOptions = {
+    class: 'modal-lg',
+    ignoreBackdropClick: true
+  };
 
   listStatus: any = [
     { id: 1, name: 'Tìm kiếm theo tên' },
     { id: 2, name: 'Tìm kiếm theo mã' },
-    { id: 3, name: 'Tìm kiếm theo quận' },
   ];
 
   constructor(private customerService: CustomerService,
     private bsModalService: BsModalService,
+    private alertMessageService: AlertMessageService,
+    private alertService: AlertService,
   ) { }
 
   ngOnInit() {
     this.customerService.get().subscribe(result => {
       if (result.responeStatus == ResponeStatusEnum.Successed) {
         this.listCustomers = result.data;
+        this.listCustomersShow = this.listCustomers;
+        this.alertService.success("Tải dữ liệu thành công!!")
+      }
+      else {
+        this.alertService.danger("Tải dữ liệu thất bại!!")
       }
     })
   }
 
   onClickCreate() {
-    this.modalRef = this.bsModalService.show(AddCustomerModalComponent, this.config)
+    this.modalRef = this.bsModalService.show(AddCustomerModalComponent, this.config);
+    this.modalRef.content.customerDataCreate.subscribe(result => {
+      if (result.responeStatus == ResponeStatusEnum.Successed) {
+        this.listCustomers.push(result.data);
+        this.listCustomersShow = this.listCustomers;
+        this.alertService.success("Thêm Thành Công!!");
+      }
+      if (result == CustomerType.Failed) {
+        this.alertService.danger("Sai định dạng hoặc chưa điền đủ dữ liệu");
+      }
+    });
+  }
+
+  onClickEdit(i: any) {
+    this.modalRef = this.bsModalService.show(UpdateCustomerModalComponent, this.config);
+    this.modalRef.content.customer = this.listCustomers[i];
+    this.modalRef.content.customerDataEdit.subscribe(result => {
+      if (result.responeStatus == ResponeStatusEnum.Successed) {
+        this.alertService.success("Sửa Thành Công!!");
+      }
+      if (result == CustomerType.Failed) {
+        this.alertService.danger("Sai định dạng hoặc chưa điền đủ dữ liệu");
+      }
+      if (result == CustomerType.NotChange) {
+        this.alertService.danger("Dữ liệu không thay đổi, vui lòng kiểm tra lại trước khi cập nhật.")
+      }
+    });
+  }
+
+  onClickCollapse(i: any) {
+    this.isExpand = !this.isExpand;
+    document.getElementsByClassName("hover-collapse").item(i).innerHTML = this.isExpand ? "Đóng －" : "Xem ＋";
   }
 }
--- FSLogistic.Web/ClientApp/src/app/admin/customer-management/customer-management.module.ts
+++ FSLogistic.Web/ClientApp/src/app/admin/customer-management/customer-management.module.ts
@@ -5,8 +5,9 @@ import { CustomerManagementRoutingModule } from './customer-management-routing.m
 import { CustomerManagementDatashowingComponent } from './customer-management-datashowing/customer-management-datashowing.component';
 import { PipeModule } from 'src/app/common/pipes/pipe.module';
 import { NgSelectModule } from '@ng-select/ng-select';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { AlertModule } from 'ngx-alerts';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,8 @@ import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
     NgSelectModule,
     FormsModule,
     NgbCollapseModule,
+    AlertModule.forRoot({maxMessages: 5, timeout: 5000, position: 'right'}),
+    ReactiveFormsModule,
   ],
   exports: [
     CustomerManagementComponent,
