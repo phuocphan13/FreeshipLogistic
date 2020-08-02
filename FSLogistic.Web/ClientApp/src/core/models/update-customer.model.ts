22d1ea2a05f542bd91143ae2b3c66aef03adfcf5
kimocheese.kimochi@gmail.com <thuanphat0611@gmail.com>
2020-08-02 17:18:55 +0700

    Story-308_Task-350 FE model for update customer

---
+++ FSLogistic.Web/ClientApp/src/core/models/update-customer.model.ts
@@ -0,0 +1,29 @@
+export class UpdateCustomer {
+    id:number;
+    code:string;
+    name: string;
+    q1: number;
+    q2: number;
+    q3: number;
+    q4: number;
+    q5: number;
+    q6: number;
+    q7: number;
+    q8: number;
+    q9: number;
+    q10: number;
+    q11: number;
+    q12: number;
+    qtd: number;
+    qgv: number;
+    qbth: number;
+    qtb: number;
+    qtp: number;
+    qbtn: number;
+    qpn: number;
+    qcc: number;
+    qhm: number;
+    qbc: number;
+    qnb: number;
+    qcg: number;
+}
